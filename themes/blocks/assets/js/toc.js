62dc0b43f2c212ab947fc29a248ce20d007cd9e6
L.B <mrlawrencelane@gmail.com>
2022-07-16 22:38:00 -0400

    toc via pure js

--- themes/blocks/assets/js/toc.js
+++ themes/blocks/assets/js/toc.js
@@ -1,24 +1,48 @@
 function styleTOC(){
 
-    var toc = document.getElementById('TableOfContents');
-    var ul = document.getElementById('TableOfContents').children[0]
-    var anchors = ul.children
-    var links = anchors.children 
+    // Get every header in the document
+    var zzz = document.getElementById('zzz');
 
-
-    ul.classList.add('tags')
-    ul.classList.add('mt-3')
-    ul.classList.add('is-flex-direction-column')
-    ul.classList.add('is-align-items-start')
-    
-    console.log(anchors)
-
-    for(var i = 0; i < anchors.length; i++){
-        anchors[i].classList.add('tag')
-        
+    var pageNodes = document.getElementsByTagName('*');
+    var headings = [];
+    for (var i = 0; i < pageNodes.length; i++) {
+        var node = pageNodes[i];
+        if (node.nodeType == 1) {
+            var nodeName = node.nodeName.toLowerCase();
+            if (nodeName == 'h2' || nodeName == 'h3') {
+                headings.push(node.id);
+            }
+        }
     }
+    console.log(headings);
+
+    // Create a TOC
+   
+    for (var i = 0; i < headings.length; i++) {
+        var heading = headings[i]; 
+        var li = document.createElement('li');
+        var link = document.createElement('a');
+        var headerNameNoDashes = heading.replace(/-/g, ' ');
+
+        link.classList.add('tag');
+        link.classList.add('mt-4')
+        link.classList.add('is-inline-block')
+        link.href = '#' + heading;
+
+        link.innerHTML = '# ' + headerNameNoDashes;
+
+        li.appendChild(link);
+        zzz.appendChild(li); 
+    }
+
+
+
+
+
     
+
+   
 
 }
 
-styleTOC() 
+styleTOC();
