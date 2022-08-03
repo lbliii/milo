189a700cbb608e5d7f4fc181e70618332c98331a
L.B <mrlawrencelane@gmail.com>
2022-08-02 20:13:29 -0400

    glossary styling

--- themes/blocks/assets/js/darkMode.js
+++ themes/blocks/assets/js/darkMode.js
@@ -4,6 +4,10 @@ function darkMode() {
    var directory = document.getElementById('directory');
    var button = document.getElementById("dark-mode-button");
    var backButton = document.getElementById("back-arrow-left");
+   var glossary = document.getElementById("glossary");
+
+
+
 
 
    // Toggle Darkmode for the directory 
@@ -15,6 +19,27 @@ function darkMode() {
     }
    }
 
+   // Toggle Darkmode for the glossary
+    if (glossary){
+        for (var i = 0; i < glossary.children.length; i++) {
+            
+            for (var j = 0; j < glossary.children[i].children.length; j++) {
+
+                const element = glossary.children[i].children[j];
+
+                if (element.classList.contains("glossary-term")) {
+                    element.classList.toggle("has-background-info");
+                    element.classList.toggle("has-background-black");
+                } else {
+
+                    element.classList.toggle("has-text-white");
+                    element.classList.toggle("has-background-black");
+
+                }
+
+            }
+        }
+    }
 
    // Toggle Darkmode for the body element 
 
