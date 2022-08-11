e27e67a28cd0a8f4dac922e107a670fa35a38fd7
L.B <mrlawrencelane@gmail.com>
2022-08-11 09:31:03 -0400

    fix for related articles

--- themes/blocks/assets/js/relatedArticles.js
+++ themes/blocks/assets/js/relatedArticles.js
@@ -1,8 +1,9 @@
 // toggles the TOC (id 'zzz') with the related articles (id 'related-articles')
-var relatedArticlesContainer = document.getElementById('zzz');
+const relatedArticlesContainer = document.getElementById('zzz');
+const showRelated = document.getElementById('see-related')
 
-if (relatedArticlesContainer) {
-    document.getElementById('see-related').addEventListener('click', function() {
+if (relatedArticlesContainer && showRelated) {
+    showRelated.addEventListener('click', function() {
         document.getElementById('zzz').classList.toggle('is-hidden');
         document.getElementById('related-articles').classList.toggle('is-hidden');
 
