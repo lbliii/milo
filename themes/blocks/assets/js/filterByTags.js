3eecea154931096cdd7ee2b9857f5b2fa15601a8
L.B <mrlawrencelane@gmail.com>
2022-08-11 12:34:52 -0400

    minor changes

--- themes/blocks/assets/js/filterByTags.js
+++ themes/blocks/assets/js/filterByTags.js
@@ -2,74 +2,79 @@ let filterSet = new Set()
 let articleSet = new Set() 
 let intersectionSet = new Set()
 
+const potentialResults = document.getElementById('potentialResults')
 
-// Push all articles to the articleSet to be filtered against the filterSet. 
-const articles = document.getElementById('potentialResults').children
+if (potentialResults) {
+    const articles = potentialResults.children
+    // Push all articles to the articleSet to be filtered against the filterSet. 
 
-for(let i = 0; i < articles.length; i++){
-    articleSet.add(articles[i])
-}
-
-// Add or remove filters to the filterSet.
-
-function Filter(filter, el){
-
-    if(!filterSet.has(filter)){
-    filterSet.add(filter);
-
-    } else if (filterSet.has(filter)){
-        filterSet.delete(filter);
+    for(let i = 0; i < articles.length; i++){
+        articleSet.add(articles[i])
     }
 
-intersect()
+    // Add or remove filters to the filterSet.
 
-el.classList.toggle('is-black');
+    function Filter(filter, el){
 
-console.log(filterSet)
-console.log(intersectionSet)
-}
-    
+        if(!filterSet.has(filter)){
+        filterSet.add(filter);
 
-// Filter the articleSet against the filterSet.
-
-function intersect(){
-
-    // Add articles to the intersectionSet if they match the filterSet.
-    articleSet.forEach(article => {
-        let tags = article.getAttribute('data-tags'). split(' ')
-        let intersection = tags.filter(tag => filterSet.has(tag))
-        if(intersection.length > 0){
-            intersectionSet.add(article)
+        } else if (filterSet.has(filter)){
+            filterSet.delete(filter);
         }
 
-    })
+    intersect()
 
-    // If the filterSet has more than one filter, make sure the intersectionSet's articles have all the filters.
+    el.classList.toggle('is-black');
 
-    if(filterSet.size > 1){
-        intersectionSet.forEach(article => {
-            let tags = article.getAttribute('data-tags').split(' ')
+    console.log(filterSet)
+    console.log(intersectionSet)
+    }
+        
+
+    // Filter the articleSet against the filterSet.
+
+    function intersect(){
+
+        // Add articles to the intersectionSet if they match the filterSet.
+        articleSet.forEach(article => {
+            let tags = article.getAttribute('data-tags'). split(' ')
             let intersection = tags.filter(tag => filterSet.has(tag))
-            if(intersection.length < filterSet.size){
-                intersectionSet.delete(article)
+            if(intersection.length > 0){
+                intersectionSet.add(article)
             }
-        } )
-    }
 
-    // If the filterset is empty, clear the intersectionSet.
-    if (filterSet.size == 0){
-        intersectionSet.clear()
-    }
-    // Update the classList to show each article in the intersectionSet.
+        })
 
-    articleSet.forEach (article => {
-        if (intersectionSet.has(article)){
-            article.classList.remove('is-hidden')
-        } else {
-            article.classList.add('is-hidden')
+        // If the filterSet has more than one filter, make sure the intersectionSet's articles have all the filters.
+
+        if(filterSet.size > 1){
+            intersectionSet.forEach(article => {
+                let tags = article.getAttribute('data-tags').split(' ')
+                let intersection = tags.filter(tag => filterSet.has(tag))
+                if(intersection.length < filterSet.size){
+                    intersectionSet.delete(article)
+                }
+            } )
         }
-    })
+
+        // If the filterset is empty, clear the intersectionSet.
+        if (filterSet.size == 0){
+            intersectionSet.clear()
+        }
+        // Update the classList to show each article in the intersectionSet.
+
+        articleSet.forEach (article => {
+            if (intersectionSet.has(article)){
+                article.classList.remove('is-hidden')
+            } else {
+                article.classList.add('is-hidden')
+            }
+        })
+    }
 }
+
+
 
 
 
