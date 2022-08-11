11a8ee874ee6c870da7cdc8f8a8dbc3813d463da
L.B <mrlawrencelane@gmail.com>
2022-08-11 09:31:58 -0400

    fix paginator

--- themes/blocks/assets/js/pagination.js
+++ themes/blocks/assets/js/pagination.js
@@ -1,7 +1,11 @@
-var paginator = document.getElementById('paginator');
+const paginator = document.getElementById('paginator');
+
+if (paginator) {
 paginator.children[0].classList.toggle('is-hidden');
 
 currentPager = paginator.children[0]
+
+}
 
 function revealPage(pager){
    lastVisiblePager = currentPager 
