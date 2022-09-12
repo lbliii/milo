b1b490ae6a91e88e680f9761aed95d319cd92dc3
L.B <mrlawrencelane@gmail.com>
2022-09-12 12:25:08 -0400

    sipe articles

--- themes/pach-emdash/assets/js/relatedArticles.js
+++ themes/pach-emdash/assets/js/relatedArticles.js
@@ -1,11 +1,1 @@
-// toggles the TOC (id 'zzz') with the related articles (id 'related-articles')
-const relatedArticlesContainer = document.getElementById('zzz');
-const showRelated = document.getElementById('see-related')
-
-if (relatedArticlesContainer && showRelated) {
-    showRelated.addEventListener('click', function() {
-        document.getElementById('zzz').classList.toggle('is-invisible');
-        document.getElementById('related-articles').classList.toggle('is-invisible');
-
-    } , false); 
-}
+ 
