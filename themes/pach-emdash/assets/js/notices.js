1bdc06d8d494a56a0ade58f2f331e51d1bc36d64
L.B <mrlawrencelane@gmail.com>
2022-09-12 15:42:46 -0400

    notices

--- themes/pach-emdash/assets/js/notices.js
+++ themes/pach-emdash/assets/js/notices.js
@@ -1,10 +1,10 @@
 // create a window event listener that checks if the user selects an element with the class delete.
 
 window.addEventListener ('click', function (event) {
-    if (event.target.classList.contains('delete')) {
-        event.target.parentElement.remove();
-        // remove child elements
-        event.target.parentElement.removeChild (event. target. parentElement. firstChild);
+    if (event.target.classList.contains('hide')) {
+        // remove the great grandparent of the element that was clicked
+        event.target.parentNode.parentNode.remove();
+
     }
 });
 
